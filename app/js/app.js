fb6096b12cf496e5b4b182d38a32b8f526afb99a
Alexander Velichko <alvi@mail.ua>
2020-12-20 21:19:57 +0200

    added mobile menu

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,23 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, {Navigation, Pagination} from 'swiper';
+
 Swiper.use([Navigation, Pagination]);
 
 document.addEventListener('DOMContentLoaded', () => {
-  const swiper = new Swiper('.swiper-container', {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+    new Swiper('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    const hamburger = document.querySelector('.header__mobile-menu');
+    const menu = document.querySelector('.main-menu');
+    if (hamburger instanceof HTMLElement && menu instanceof HTMLElement) {
+        hamburger.addEventListener('click', () => menu.classList.toggle('active'));
+    }
 })
